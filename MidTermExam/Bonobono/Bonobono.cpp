a0eedda86ee247521c6b119ebafa98139e9a7dfb
LEEDDONG <ehdgus2580@naver.com>
2023-11-13 19:10:45 +0900

    10주차 중간고사 과제

---
+++ MidTermExam/Bonobono/Bonobono.cpp
@@ -0,0 +1,267 @@
+﻿
+#include "../Ryan/yuhanCG.h"
+#define BOX_BUTTON_ID 1
+#define CIRCLE_BUTTON_ID 2
+#define BONOBONO_BUTTON_ID 3
+#define RYAN_BUTTON_ID 4
+#define CUBE_BUTTON_ID 5
+bool isBoxVisible = false;
+bool isCirclesible = false;
+bool isDragging = false;
+bool isMoving = false;
+bool isspacebar = false;
+bool isbono = false;
+bool isryan = false;
+int blink = 0;
+
+POINT startDrag;
+POINT startMove;
+POINT circleCenter;
+
+RECT drawArea = { 16, 96, 764, 424 };
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    RECT Box = { 8, 8, 772, 432 };
+
+    switch (message) {
+    case WM_COMMAND:
+        isBoxVisible = false;
+        isCirclesible = false;
+        isbono = false;
+        if (LOWORD(wParam) == BOX_BUTTON_ID) {
+            isBoxVisible = true;
+        }
+        if (LOWORD(wParam) == CIRCLE_BUTTON_ID) {
+            isCirclesible = true;
+        }
+        if (LOWORD(wParam) == BONOBONO_BUTTON_ID) {
+            isbono = true;
+            isryan = false;
+        }
+
+        if (LOWORD(wParam) == RYAN_BUTTON_ID) {
+            isbono = false;
+            isryan = true;
+        }
+        InvalidateRect(hWnd, NULL, TRUE);
+        SetFocus(hWnd);
+        break;
+    case WM_LBUTTONDOWN:
+        if (isryan) {
+            startDrag.x = LOWORD(lParam);
+            startDrag.y = HIWORD(lParam);
+            isDragging = true;
+            isMoving = false;
+        }
+        break;
+    case WM_RBUTTONDOWN:
+        if (isryan) {
+            startDrag.x = LOWORD(lParam);
+            startDrag.y = HIWORD(lParam);
+            isDragging = true;
+            isMoving = false;
+        }
+        break;
+
+    case WM_MOUSEMOVE:
+        if (isDragging && isryan) {
+            HDC hdc = GetDC(hWnd);
+
+            // Update the startMove point based on the drag movement
+            startMove.x = LOWORD(lParam);
+            startMove.y = HIWORD(lParam);
+
+            // Check if the cursor is within the drawArea
+            if (PtInRect(&drawArea, startMove)) {
+                // Calculate the new rectangle based on the drag movement
+                int newRectLeft = startDrag.x;
+                int newRectTop = startDrag.y;
+                int newRectRight = startDrag.x ;
+                int newRectBottom = startDrag.y ;
+
+                // Redraw the window only if the cursor is within drawArea
+                InvalidateRect(hWnd, &drawArea, TRUE);
+                UpdateWindow(hWnd);
+
+                // Draw the rectangle
+                DrawRyan(hWnd, hdc, newRectLeft, newRectTop, newRectRight, newRectBottom);
+            }
+            else {
+                // The cursor is outside drawArea, stop drawing
+                isDragging = false;
+                isMoving = false;
+            }
+
+            ReleaseDC(hWnd, hdc);
+        }  break;
+
+
+    case WM_LBUTTONUP:
+        if (isryan) {
+            POINT clickPoint;
+            clickPoint.x = LOWORD(lParam);
+            clickPoint.y = HIWORD(lParam);
+
+
+            isDragging = false;
+            isMoving = false;
+
+        }
+        break;
+
+    case WM_RBUTTONUP:
+        if (isryan) {
+            POINT clickPoint;
+            clickPoint.x = LOWORD(lParam);
+            clickPoint.y = HIWORD(lParam);
+
+
+            isDragging = false;
+            isMoving = false;
+
+        }
+        break;
+    case WM_KEYDOWN: // 스페이스바를 눌렀을 때
+        if (wParam == VK_SPACE) {
+           // isspacebar = true;
+            blink = 1;
+            if (isbono) {
+                InvalidateRect(hWnd, NULL, TRUE);
+            }
+        }
+
+
+
+        break;
+    case WM_SETCURSOR:
+    {
+        // 현재 마우스 커서 위치를 얻습니다.
+        POINT cursorPos;
+        GetCursorPos(&cursorPos);
+        ScreenToClient(hWnd, &cursorPos);
+
+        // 커서 위치가 drawArea 영역 내에 있는지 확인합니다.
+        if (PtInRect(&drawArea, cursorPos)) {
+            // drawArea 영역 내에 있다면 십자가 모양의 커서로 설정합니다.
+            SetCursor(LoadCursor(NULL, IDC_CROSS));
+        }
+        else {
+            // drawArea 영역 밖에 있다면 원래 커서로 설정합니다.
+            SetCursor(LoadCursor(NULL, IDC_ARROW));
+        }
+
+        return TRUE; // 커서를 변경했음을 알립니다.
+    }
+    break;
+
+    case WM_KEYUP:
+       // isspacebar = false;
+        blink = 0;
+        if (isbono) {
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        break;
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 0));
+        HBRUSH hBackgroundBrush = CreateSolidBrush(RGB(255, 240, 200));
+        FillRect(hdc, &ps.rcPaint, hBackgroundBrush);
+        DeleteObject(hBackgroundBrush);
+        FrameRect(hdc, &Box, hBrush);
+        FrameRect(hdc, &drawArea, hBrush);
+        FillRect(hdc, &drawArea, CreateSolidBrush(RGB(255, 255, 255)));
+
+        DeleteObject(hBrush);
+
+        if (isbono) {
+            // 스페이스바를 눌렀을 때 윙크하는 부분
+            DrawBonobono(hWnd, hdc, blink);
+        }
+        if (isryan) {
+            // Calculate the new rectangle based on the drag movement
+            int newRectLeft = startDrag.x;
+            int newRectTop = startDrag.y;
+            int newRectRight = startDrag.x + (startMove.x - startDrag.x);
+            int newRectBottom = startDrag.y + (startMove.y - startDrag.y);
+
+            DrawRyan(hWnd, hdc, newRectLeft, newRectTop, newRectRight, newRectBottom);
+        }
+        EndPaint(hWnd, &ps);
+    }
+                 break;
+
+
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
+    HWND hWnd;
+    HWND hButton1, hButton2, hButton3, hButton4, hButton5;
+
+    WNDCLASSEX wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = L"ButtonWindowClass";
+    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex)) {
+        return 1;
+    }
+
+
+    hWnd = CreateWindow(
+        L"ButtonWindowClass", L"Win32 Button Example", WS_OVERLAPPEDWINDOW,
+        100, 100, 800, 480, NULL, NULL, hInstance, NULL);
+    SetWindowLong(hWnd, GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) & ~(WS_SIZEBOX | WS_MAXIMIZEBOX));
+
+    if (!hWnd) {
+        return FALSE;
+    }
+
+    hButton1 = CreateWindow(
+        L"BUTTON", L"Box", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        16, 16, 130, 64, hWnd, (HMENU)BOX_BUTTON_ID, hInstance, NULL);
+
+    hButton2 = CreateWindow(
+        L"BUTTON", L"Circle", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        162, 16, 130, 64, hWnd, (HMENU)CIRCLE_BUTTON_ID, hInstance, NULL);
+
+    hButton3 = CreateWindow(
+        L"BUTTON", L"Bonobono", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        308, 16, 130, 64, hWnd, (HMENU)BONOBONO_BUTTON_ID, hInstance, NULL);
+    hButton4 = CreateWindow(
+        L"BUTTON", L"Ryan", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        454, 16, 130, 64, hWnd, (HMENU)RYAN_BUTTON_ID, hInstance, NULL);
+    hButton5 = CreateWindow(
+        L"BUTTON", L"Cube", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        620, 16, 130, 64, hWnd, (HMENU)CUBE_BUTTON_ID, hInstance, NULL);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
---
+++ MidTermExam/Ryan/yuhanCG.cpp
@@ -0,0 +1,242 @@
+#include "yuhanCG.h"
+
+ 
+void DrawBonobono(HWND hWnd, HDC hdc, int blink) {
+    int faceLeft = 220;
+    int faceTop = 100;
+    int faceRight = 480;
+    int faceBottom = 350;
+    // 얼굴 영역을 그리기
+    HBRUSH hFaceBrush = CreateSolidBrush(RGB(127, 200, 255)); // 흰색 얼굴
+    SelectObject(hdc, hFaceBrush);
+    Ellipse(hdc, faceLeft, faceTop, faceRight, faceBottom);
+    DeleteObject(hFaceBrush);
+    if (blink) {
+        // 왼쪽찡긋
+        int wstartX = 250;
+        int wstartY = 200;
+        int wendX = 230;
+        int wendY = 190;
+
+        MoveToEx(hdc, wstartX, wstartY, NULL); // 시작점 이동
+        LineTo(hdc, wendX, wendY); // 대각선 
+
+        int wrstartX = 250;
+        int wrstartY = 200;
+        int wrendX = 230;
+        int wrendY = 235;
+
+        MoveToEx(hdc, wrstartX, wrstartY, NULL); // 시작점 이동
+        LineTo(hdc, wendX, wrendY); // 대각선 그리기
+
+        // 왼쪽찡긋
+        int wkrstartX = 450;
+        int wkrstartY = 200;
+        int wkrendX = 470;
+        int wkrendY = 190;
+
+        MoveToEx(hdc, wkrstartX, wkrstartY, NULL); // 시작점 이동
+        LineTo(hdc, wkrendX, wkrendY); // 대각선 
+
+        int wklstartX = 450;
+        int wklstartY = 200;
+        int wklendX = 470;
+        int wklendY = 235;
+
+        MoveToEx(hdc, wklstartX, wklstartY, NULL); // 시작점 이동
+        LineTo(hdc, wklendX, wklendY); // 대각선 그리기
+
+    }
+    else {
+
+
+        // 눈 그리기
+        int eyeWidth = 6;   // 눈 가로 반지름
+        int eyeHeight = 12; // 눈 세로 반지름
+        int leftEyeX = 250;
+        int rightEyeX = 450;
+        int eyeY = 200;
+
+        HBRUSH hEyeBrush = CreateSolidBrush(RGB(0, 0, 0)); // 눈 색상 (검정색)
+        SelectObject(hdc, hEyeBrush);
+        Ellipse(hdc, leftEyeX - eyeWidth, eyeY - eyeHeight, leftEyeX + eyeWidth, eyeY + eyeHeight);
+        Ellipse(hdc, rightEyeX - eyeWidth, eyeY - eyeHeight, rightEyeX + eyeWidth, eyeY + eyeHeight);
+        DeleteObject(hEyeBrush);
+
+        // 눈흰색 그리기
+        int wEyeWidth = 3;    // 흰색 눈 가로 반지름
+        int wEyeHeight = 4;   // 흰색 눈 세로 반지름
+        int leftWEyeX = 250;
+        int rightWEyeX = 450;
+        int weyeY = 195;
+
+        HBRUSH hwEyeBrush = CreateSolidBrush(RGB(255, 255, 255)); // 눈 색상 (흰색)
+        SelectObject(hdc, hwEyeBrush);
+        Ellipse(hdc, leftWEyeX - wEyeWidth, weyeY - wEyeHeight, leftWEyeX + wEyeWidth, weyeY + wEyeHeight);
+        Ellipse(hdc, rightWEyeX - wEyeWidth, weyeY - wEyeHeight, rightWEyeX + wEyeWidth, weyeY + wEyeHeight);
+        DeleteObject(hwEyeBrush);
+    }
+    // 입술 그리기
+    int lipLeft = 335;
+    int lipRight = 365;
+    int lipTop = 250;
+    int lipBottom = 300;
+
+    HBRUSH hLipBrush = CreateSolidBrush(RGB(255, 150, 255)); // 입술 색상 
+    SelectObject(hdc, hLipBrush);
+    Ellipse(hdc, lipLeft, lipTop, lipRight, lipBottom);
+    DeleteObject(hLipBrush);
+    // 코1
+    int noseLeft = 300;
+    int noseRight = 350;
+    int noseTop = 220;
+    int noseBottom = 270;
+
+    HBRUSH hnose1Brush = CreateSolidBrush(RGB(255, 255, 255)); // (코1)
+    SelectObject(hdc, hnose1Brush);
+    Ellipse(hdc, noseLeft, noseTop, noseRight, noseBottom);
+    DeleteObject(hnose1Brush);
+    // 코2
+    int rnoseLeft = 350;
+    int rnoseRight = 400;
+    int rnoseTop = 220;
+    int rnoseBottom = 270;
+
+    HBRUSH hnose2Brush = CreateSolidBrush(RGB(255, 255, 255)); //코2
+    SelectObject(hdc, hnose2Brush);
+    Ellipse(hdc, rnoseLeft, rnoseTop, rnoseRight, rnoseBottom);
+    DeleteObject(hnose2Brush);
+    // 코3
+    int cnoseLeft = 335;
+    int cnoseRight = 365;
+    int cnoseTop = 210;
+    int cnoseBottom = 240;
+
+    HBRUSH hnose3Brush = CreateSolidBrush(RGB(0, 0, 0)); //코2
+    SelectObject(hdc, hnose3Brush);
+    Ellipse(hdc, cnoseLeft, cnoseTop, cnoseRight, cnoseBottom);
+    DeleteObject(hnose3Brush);
+    // 수염1
+    int startX = 290;
+    int startY = 280;
+    int endX = 310;
+    int endY = 250;
+
+    MoveToEx(hdc, startX, startY, NULL); // 시작점 이동
+    LineTo(hdc, endX, endY); // 대각선 그리기
+     // 수염2
+    int start2X = 275;
+    int start2Y = 230;
+    int end2X = 310;
+    int end2Y = 235;
+
+    MoveToEx(hdc, start2X, start2Y, NULL); // 시작점 이동
+    LineTo(hdc, end2X, end2Y); // 대각선 그리기
+     // 수염3
+    int start3X = 410;
+    int start3Y = 275;
+    int end3X = 390;
+    int end3Y = 250;
+
+    MoveToEx(hdc, start3X, start3Y, NULL); // 시작점 이동
+    LineTo(hdc, end3X, end3Y); // 대각선 그리기
+     // 수염4
+    int start4X = 415;
+    int start4Y = 220;
+    int end4X = 385;
+    int end4Y = 230;
+
+    MoveToEx(hdc, start4X, start4Y, NULL); // 시작점 이동
+    LineTo(hdc, end4X, end4Y); // 대각선 그리기
+}
+
+void DrawRyan(HWND hWnd, HDC hdc, int left, int top, int right, int bottom) {
+    // Calculate the center and radius using the provided parameters
+    int centerX = (left + right) / 2;
+    int centerY = (top + bottom) / 2;
+    int radius = min((right - left) / 2, (bottom - top) / 2);
+
+    HBRUSH hBrush = CreateSolidBrush(RGB(255, 200, 15));
+    SelectObject(hdc, hBrush);
+
+    int earRadius = radius / 3;
+
+    // Left ear
+    int leftearX = centerX - earRadius * 2;
+    int leftearY = centerY - earRadius * 2;
+    HBRUSH hLeftearBrush = CreateSolidBrush(RGB(255, 200, 15));
+    SelectObject(hdc, hLeftearBrush);
+    Ellipse(hdc, leftearX - earRadius, leftearY - earRadius, leftearX + earRadius, leftearY + earRadius);
+    DeleteObject(hLeftearBrush);
+
+    // Right ear
+    int rightearX = centerX - earRadius * -2;
+    int rightearY = centerY - earRadius * 2;
+    HBRUSH hRIGHTearBrush = CreateSolidBrush(RGB(255, 200, 15));
+    SelectObject(hdc, hRIGHTearBrush);
+    Ellipse(hdc, rightearX - earRadius, rightearY - earRadius, rightearX + earRadius, rightearY + earRadius);
+    DeleteObject(hRIGHTearBrush);
+
+    // Face
+    Ellipse(hdc, centerX - radius, centerY - radius, centerX + radius, centerY + radius);
+
+    // Eyes
+    int eyeRadius = radius / 12;
+
+    // Left eye
+    int leftEyeX = centerX - eyeRadius * 7;
+    int leftEyeY = centerY - eyeRadius;
+    HBRUSH hLeftEyeBrush = CreateSolidBrush(RGB(0, 0, 0)); // Black color for left eye
+    SelectObject(hdc, hLeftEyeBrush);
+    Ellipse(hdc, leftEyeX - eyeRadius, leftEyeY - eyeRadius, leftEyeX + eyeRadius, leftEyeY + eyeRadius);
+    DeleteObject(hLeftEyeBrush);
+
+    // Right eye
+    int rightEyeX = centerX + eyeRadius * 7;
+    int rightEyeY = centerY - eyeRadius;
+    HBRUSH hRightEyeBrush = CreateSolidBrush(RGB(0, 0, 0)); // Black color for right eye
+    SelectObject(hdc, hRightEyeBrush);
+    Ellipse(hdc, rightEyeX - eyeRadius, rightEyeY - eyeRadius, rightEyeX + eyeRadius, rightEyeY + eyeRadius);
+    DeleteObject(hRightEyeBrush);
+
+    // Eyebrows
+    int eyebrowWidth = radius / 12;
+    int eyebrowHeight = radius / 12;
+
+    // Left eyebrows above the eye
+    int leftEyebrowStartX = centerX - eyebrowWidth * 8.5;
+    int leftEyebrowStartY = centerY - eyebrowHeight * 4;
+    int leftEyebrowEndX = leftEyebrowStartX + eyebrowWidth * 3;
+    int leftEyebrowEndY = leftEyebrowStartY;
+
+    MoveToEx(hdc, leftEyebrowStartX, leftEyebrowStartY, NULL);
+    LineTo(hdc, leftEyebrowEndX, leftEyebrowEndY);
+
+    // Right eyebrows above the eye
+    int rightEyebrowStartX = centerX + eyebrowWidth * 6;
+    int rightEyebrowStartY = centerY - eyebrowHeight * 4;
+    int rightEyebrowEndX = rightEyebrowStartX + eyebrowWidth * 3;
+    int rightEyebrowEndY = rightEyebrowStartY;
+
+    MoveToEx(hdc, rightEyebrowStartX, rightEyebrowStartY, NULL);
+    LineTo(hdc, rightEyebrowEndX, rightEyebrowEndY);
+
+    // Nose
+    int RadiusNose = radius / 6;
+
+    // Left nose
+    int leftNoseX = centerX - RadiusNose;
+    int leftNoseY = centerY - RadiusNose * (-1);
+    HBRUSH hLeftNoseBrush = CreateSolidBrush(RGB(255, 255, 255));
+    SelectObject(hdc, hLeftNoseBrush);
+    Ellipse(hdc, leftNoseX - RadiusNose, leftNoseY - RadiusNose, leftNoseX + RadiusNose, leftNoseY + RadiusNose);
+    DeleteObject(hLeftNoseBrush);
+
+    // Right nose
+    int rightNoseX = centerX - RadiusNose * -1;
+    int rightNoseY = centerY - RadiusNose * (-1);
+    HBRUSH hRightNoseBrush = CreateSolidBrush(RGB(255, 255, 255));
+    SelectObject(hdc, hRightNoseBrush);
+    Ellipse(hdc, rightNoseX - RadiusNose, rightNoseY - RadiusNose, rightNoseX + RadiusNose, rightNoseY + RadiusNose);
+    DeleteObject(hRightNoseBrush);
+}
